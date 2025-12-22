b25eee97e478cd4952384e5d18ccb9edc421a932
Aespekson <john6610@yandex.com>
2025-12-21 23:54:31 -0500

    Quick updates to everything else. Nothing conceptually different. Also scripting for instruction memory.

--- Counter/tb_counter.v
+++ Counter/tb_counter.v
@@ -1,21 +1,11 @@
-module tb;
-
-reg clk = 0;
-reg rst = 1;
-wire [3:0] count;
-
 // Remember to invoke simulator from ../RV32-Base, not a lower directory or includes may not work properly
 `include "../tb_sanitation/tb_standard.vh"
 
-counter uut (
-    .clk(clk),
-    .rst(rst),
-    .count(count)
-);
+module tb;
 
 initial begin
     $dumpfile("Counter/wave.vcd");
-    $dumpvars(0, tb);
+    $dumpvars(0, standard);
 end
 
 endmodule
--- Run_Scripting/Run Counter.sh
+++ Run_Scripting/Run Counter.sh
@@ -2,7 +2,5 @@
 cd ..
 
 iverilog -g2012 -o Counter/sim -ICounter -Itb_sanitation Counter/tb_counter.v Counter/counter.v
-
 vvp Counter/sim
-
 gtkwave Counter/wave.vcd
