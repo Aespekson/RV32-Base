c963d73254998913e19c3e9326c1824e3c7b088f
Aespekson <john6610@yandex.com>
2026-01-12 09:47:44 -0500

    Reliance on data width and num_register macros reintroduced. I do not anticipate having to use them, but it may be nice if necessary. Module declarations fixed. First check created. Variables initialized and declared.

--- Register_File/register_file.v
+++ Register_File/register_file.v
@@ -1,11 +1,33 @@
-
+`define DATA_WIDTH 32
+`define NUM_REGISTER 32 //Some reliance on these macros were removed from other modules, but it has the value of adjusting automatically if I wish to change them, so those will be reintroduced there later.
 
 module register_file(
   input wire clk,
   input wire we,
   input wire rst,
-  input wire [4:0] rd_addr,
-  input wire [4:0] rs1_addr.
-  input wire [4:0] rs2_addr,
-  output wire [31:0] rs1,
-  output wire [31:0] rs2);
+  input wire [$clog2(`NUM_REGISTER)-1:0] rd_addr,
+  input wire [`DATA_WIDTH-1:0] rd,
+  input wire [$clog2(`NUM_REGISTER)-1:0] rs1_addr.
+  input wire [$clog2(`NUM_REGISTER)-1:0] rs2_addr,
+  output wire [`DATA_WIDTH-1:0] rs1,
+  output wire [`DATA_WIDTH-1:0] rs2);
+
+
+  reg [31:0] registers [31:0]; //Registers declared as nested (2-dim) array. Do not be mistaken; the former 31 comes from the number of registers (32) while the latter comes from the data width of 32.
+    integer i;
+
+    initial begin //Important that this only valid in simulation for testing
+        for (i = 0; i < `NUM_REGISTER; i = i + 1) begin //Not really necessary but avoids leaving things undefined.
+            registers[i] = 32'h00000000;
+        end
+        registers[0] = 1;
+        registers[1] = 2;
+        registers[2] = 3;
+        registers[29] = 3;
+        registers[30] = 2;
+        registers[31] = 1;
+    end
+
+
+
+  endmodule
--- Register_File/register_file_tb.v
+++ Register_File/register_file_tb.v
@@ -1,24 +1,41 @@
+`define DATA_WIDTH 32
+`define NUM_REGISTER 32 //Some reliance on these macros were removed from other modules, but it has the value of adjusting automatically if I wish to change them, so those will be reintroduced there later.
 
+`define assert(signal, value) \
+        if (signal !== value) $finish;
 
 module register_file_tb;
 
 reg clk;
 wire we,
 wire rst,
-wire [4:0] rd_addr,
-wire [4:0] rs1_addr.
-wire [4:0] rs2_addr,
-wire [31:0] rs1,
-wire [31:0] rs2
+wire [$clog2(`NUM_REGISTER)-1:0] rd_addr,
+wire [$clog2(`NUM_REGISTER)-1:0] rs1_addr.
+wire [$clog2(`NUM_REGISTER)-1:0] rs2_addr,
+wire [`DATA_WIDTH-1:0] rs1,
+wire [`DATA_WIDTH-1:0] rs2
 
 register_file dut (
-    .clk(clk)
-    .we(we)
-    .rst(rst)
-    .rd_addr(rd_addr)
-    .rs1_addr(rs1_addr)
-    .rs2_addr(rs2_addr)
-    .rs1(rs1)
+    .clk(clk),
+    .we(we),
+    .rst(rst),
+    .rd_addr(rd_addr),
+    .rd(rd),
+    .rs1_addr(rs1_addr),
+    .rs2_addr(rs2_addr),
+    .rs1(rs1),
     .rs2(rs2))
 
+  initial begin
+    clk = 0;
+    we = 0;
+    rst = 0;
+    rs1_addr = 5'b00000;
+    rs2_addr = 5'b00000;
+  #10
+    `assert(dut.rs1,32'h00000000)
+    `assert(dut.rs1,32'h00000000)
+  end
 
+  always #5 clk = ~clk;
+endmodule
