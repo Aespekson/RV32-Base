3e208581d2b711654fd8411ec6e41a3a6e7f07e3
aespekson <john6610@yandex.com>
2026-05-17 15:36:19 -0400

    Created preliminary tb to verify branch logic.

--- Branch_Log/branch_log.v
+++ Branch_Log/branch_log.v
@@ -1,13 +1,5 @@
-`define DATA_WIDTH 32
-
-`define BRANCH_BEQ 3'b000 // Branch Equal
-`define BRANCH_BNE 3'b001 // Branch Not Equal
-`define BRANCH_BLT 3'b100 // Branch Less Than
-`define BRANCH_BGE 3'b101 // Branch Greater Than Or Equal
-`define BRANCH_BLTU 3'b110 // Branch Less Than Unsigned
-`define BRANCH_BGEU 3'b111 // Branch Greater Than Or Equal Unsigned
-`define BRANCH_JAL_JALR 3'b010 // Jump in case of JAL or JALR instruction
-// opcodes above are from funct3 field. Be careful with 010. It is not specified as a branch code in specs
+// All definitions moved to definitions.vh
+`include "definitions.vh"
 
 module branch_log ( // In short, determines whether a branch should be taken based on certain conditions. The condition is specified via operation code, similar to ALU, and evaluated based on operands a and b.
     input wire i_branch, // Effectively an enable signal
--- Branch_Log/branch_log_tb.v
+++ Branch_Log/branch_log_tb.v
@@ -1,7 +1,8 @@
-`define assert(signal, value) \
-        if (signal !== value) $finish;
+`include "definitions.vh"
 
-
+// Unique to this tb rn. Will not move to definitions for now.
+`define incrementpass \
+        passed++;
 
 module  branch_unit_tb;
 
@@ -21,6 +22,8 @@ module  branch_unit_tb;
     .o_take(o_take)
   );
 
+  reg [2:0] passed; //Counter intended to count the number of operations that work, in order tested in the testbench
+
   initial begin
     clk = 1'b0;
   end
@@ -28,9 +31,85 @@ module  branch_unit_tb;
   always #5 clk = ~clk;
 
   initial begin
-
+    passed = 0;
+    i_branch = 1'b1;
+    i_a = 32'hA0000000;
+    i_b = 32'hA0000000;
+    i_branch_op = `BRANCH_BEQ;
+    #10
+      `assert(dut.o_take, 1'b1);
+    i_b = 0;
+    #10
+      `assert(dut.o_take, 1'b0);// Tests equal op in case that arguments are equal and not equal
+      `incrementpass //System Verilog. If testing with pure verilog interpreter, change. Others marked with //* and number of ops tested so you can find them easier
+    i_branch = 0;
     #10;
+      `assert(dut.o_take, 1'b0);// Tests that enable signal (i_branch) works properly. Not an operation
+    i_branch = 1;
+    i_b = 32'hA0000000;
+    i_branch_op = `BRANCH_BNE;
+    #10
       `assert(dut.o_take, 1'b0);
+    i_b = 0;
+    #10
+      `assert(dut.o_take, 1'b1);
+    i_b = 32'hA0000000;
+    i_a = 0;
+    #10
+      `assert(dut.o_take, 1'b1);
+      `incrementpass//* 2
+    i_branch_op = `BRANCH_BLT;
+    #10
+      `assert(dut.o_take, 1'b0);
+    i_b = 0;
+    #10
+      `assert(dut.o_take, 1'b0);
+    i_a = 32'hB000000B;
+    #10
+      `assert(dut.o_take, 1'b1);
+      `incrementpass//* 3
+    i_branch_op = `BRANCH_BGE;
+    #10
+      `assert(dut.o_take, 1'b0);
+    i_b = i_a;
+    #10
+      `assert(dut.o_take, 1'b1);
+    i_a = 0;
+    #10
+      `assert(dut.o_take, 1'b1);
+      `incrementpass//* 4
+    i_branch_op = `BRANCH_BLTU;
+    i_a = 0;
+    #10
+      `assert(dut.o_take, 1'b1);
+    i_b = 0;
+    #10
+      `assert(dut.o_take, 1'b0);
+    i_a = 32'hB000000B;
+    #10
+      `assert(dut.o_take, 1'b0);
+      `incrementpass//* 5
+    i_branch_op = `BRANCH_BGEU;
+    #10
+      `assert(dut.o_take, 1'b1);
+    i_b = i_a;
+    #10
+      `assert(dut.o_take, 1'b1);
+    i_a = 0;
+    #10
+      `assert(dut.o_take, 1'b0);
+      `incrementpass//* 6
+    i_branch_op = `BRANCH_JAL_JALR;
+    #10
+      `assert(dut.o_take, 1'b1)
+      `incrementpass//* 7
+    #10;
+    i_branch_op = 3'b011;// Testing the one illegal opcode; not an operation; should default to 0
+    #10
+      `assert(dut.o_take, 1'b0)
+      `incrementpass //* 8
+      //Even though it isn't an operation, I feel the need to signal it is complete some way
+    #10
     $finish;
 
   end
