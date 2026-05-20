897f8673e211ae761d77216ab1e2cd0dedef7406
aespekson <john6610@yandex.com>
2026-05-20 15:17:56 -0400

    Finished port declarations. Will begin working anyway.

--- Instruct_Decode/decode.v
+++ Instruct_Decode/decode.v
@@ -1,39 +1,21 @@
-// Note on operation codes (numbers right of definition)
-// First four bits define class (arithmetic, logic, and, for lack of better terminology, setting operations (abs, SLT, SLL, etc))
-// Latter four bits specify sub operation within each class (add, sub, and, etc)
-
-// RISC-V ALU Operations
-`define OP_ALU_NOP    8'b00000000 // Zero/"Universal Identity" operation
-`define OP_ALU_ADD    8'b00010001 // Add
-`define OP_ALU_SUB    8'b00010010 // Subtract
-`define OP_ALU_AND    8'b00100001 // Bitwise AND
-`define OP_ALU_OR     8'b00100010 // Bitwise OR
-`define OP_ALU_XOR    8'b00100011 // Bitwise XOR
-`define OP_ALU_SLT    8'b00110001 // Set Less Than (signed)
-//`define OP_ALU_SLTU   8'b00110010 // Set Less Than (unsigned)  //Currently disabled. Sticking with just SLT for now
-`define OP_ALU_SLL    8'b00110011 // Shift Left Logical
-`define OP_ALU_SRL    8'b00110100 // Shift Right Logical
-`define OP_ALU_SRA    8'b00110101 // Shift Right Arithmetic
-
-// Additional operations outside basics (though still pretty basic). Includes RV32M operations (MUL,DIV) and custom operations
-// Will likely be expanded later
-`define OP_ALU_INV    8'b00100100 // Bitwise Inversion (NOT operation)
-`define OP_ALU_MUL    8'b00010011 // Multiplication
-`define OP_ALU_DIV    8'b00010100 // Division
-`define OP_ALU_MOD    8'b00010101 // Modulus
-
-`define DATA_WIDTH 32
-
-`define INST_WIDTH 32 //32 bit instructions
-
-//Going to need all definitions above + more
+`include "definitions.vh"
 
 module decode (
     input wire[`INST_WIDTH-1:0] inst,
 
-    output reg [7:0] o_alu_op,
-    output reg [31:0] o_alu_a,
-    output reg [31:0] o_alu_b,
+    output reg [`FUNCT7_WIDTH-1:0] o_alu_op,
+    output reg [`DATA_WIDTH-1:0] o_alu_a,
+    output reg [`DATA_WIDTH-1:0] o_alu_b,
+
+    output reg o_branch,
+    output reg [2:0] o_branch_op,
+    output wire [`FUNCT7_WIDTH-1:0] o_opcode,
+    output reg [1:0] o_result_mux,
+    output reg o_reg_write,
+    output wire [$clog2(`NUM_REGISTER)-1:0] o_rs1_addr,
+    output wire [$clog2(`NUM_REGISTER)-1:0] o_rs2_addr,
+    output wire [$clog2(`NUM_REGISTER)-1:0] o_rd_addr,
+
     output reg o_mem_write
 );
 
--- Instruct_Decode/decode_tb.v
+++ Instruct_Decode/decode_tb.v
@@ -1,28 +1,37 @@
-`define assert(signal, value) \
-        if (signal !== value) $finish;
-
 `timescale 1ns/1ns
-
-`define INST_WIDTH 32
 
 module  decode_tb;
 
   reg clk;
   reg [`INST_WIDTH-1:0] inst = 32'h00000000;
 
-  wire [7:0] o_alu_op;
+  wire [`FUNCT7_WIDTH-1:0] o_alu_op;
   wire [31:0] o_alu_a;
   wire [31:0] o_alu_b;
+  wire o_branch;
+  wire [2:0] o_branch_op;
   wire o_mem_write;
-//Add more wire outputs after completing other components that require them
-
+  wire [`FUNCT7_WIDTH-1:0] o_opcode;
+  wire [1:0] o_result_mux;
+  wire o_reg_write;
+  wire [$clog2(`NUM_REGISTER)-1:0] o_rs1_addr;
+  wire [$clog2(`NUM_REGISTER)-1:0] o_rs2_addr;
+  wire [$clog2(`NUM_REGISTER)-1:0] o_rd_addr;
 
   decode dut (
     .inst(inst),
     .o_alu_op(o_alu_op),
     .o_alu_a(o_alu_a),
     .o_alu_b(o_alu_b),
-    .o_mem_write(o_mem_write)
+    .o_mem_write(o_mem_write),
+    .o_branch(o_branch),
+    .o_branch_op(o_branch_op),
+    .o_opcode(o_opcode),
+    .o_result_mux(o_result_mux),
+    .o_reg_write(o_reg_write),
+    .o_rs1_addr(o_rs1_addr),
+    .o_rs2_addr(o_rs2_addr),
+    .o_rd_addr(o_rd_addr)
   );
 
   initial begin
