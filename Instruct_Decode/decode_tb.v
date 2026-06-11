8fe0e978d8101d452dd8fd6ed3bae798d2c58cd9
aespekson <john6610@yandex.com>
2026-06-11 19:43:17 -0400

    Changed signals to fit decode.v where necessary. Also other miscellany. Will include preliminary checks tomorrow.

--- Instruct_Decode/decode_tb.v
+++ Instruct_Decode/decode_tb.v
@@ -1,22 +1,22 @@
 `timescale 1ns/1ns
+`include "definitions.vh"
 
-module  decode_tb;
+module decode_tb;
+  reg [`INST_WIDTH-1:0]       inst;
 
-  reg clk;
-  reg [`INST_WIDTH-1:0] inst = 32'h00000000;
-
-  wire [`FUNCT7_WIDTH-1:0] o_alu_op;
-  wire [`DATA_WIDTH-1:0] o_alu_a;
-  wire [`DATA_WIDTH-1:0] o_alu_b;
-  wire o_branch;
-  wire [2:0] o_branch_op;
-  wire o_mem_write;
-  reg [6:0] o_opcode;
-  wire [1:0] o_result_mux;
-  wire o_reg_write;
-  reg [$clog2(`NUM_REGISTER)-1:0] o_rs1_addr;
-  reg [$clog2(`NUM_REGISTER)-1:0] o_rs2_addr;
-  reg [$clog2(`NUM_REGISTER)-1:0] o_rd_addr;
+  wire [`ALU_OP_WIDTH-1:0]    o_alu_op;
+  wire [`DATA_WIDTH-1:0]      o_alu_a;
+  wire [`DATA_WIDTH-1:0]      o_alu_b;
+  wire                        o_branch;
+  wire [2:0]                  o_branch_op;
+  wire [6:0]                  o_opcode; //Remove here if removed in decode.v
+  wire [1:0]                  o_result_mux;
+  wire                        o_reg_write;
+  wire [`REG_ADDR_WIDTH-1:0]  o_rs1_addr;
+  wire [`REG_ADDR_WIDTH-1:0]  o_rs2_addr;
+  wire [`REG_ADDR_WIDTH-1:0]  o_rd_addr;
+  wire [`DATA_WIDTH-1:0]      o_imm;
+  wire                        o_mem_write;
 
   decode dut (
     .inst(inst),
@@ -31,22 +31,18 @@ module  decode_tb;
     .o_reg_write(o_reg_write),
     .o_rs1_addr(o_rs1_addr),
     .o_rs2_addr(o_rs2_addr),
-    .o_rd_addr(o_rd_addr)
+    .o_rd_addr(o_rd_addr),
+    .o_imm(o_imm)
   );
 
   initial begin
-    clk = 1'b0;
-  end
-
-  always #5 clk = ~clk;
-
-  initial begin
-    inst = 32'h00000000;
+  inst = 32'h00000000;
   #10
     inst = 32'hAAAAAAAA;
   #11
-    $finish;
 
+    $display("All Checks Passed."); // When we put checks in here, of course
+    $finish;
   end
 
   initial begin
