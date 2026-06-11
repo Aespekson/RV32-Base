8d64f2fd5f777fc1ba20c8577629058a7528167f
aespekson <john6610@yandex.com>
2026-06-11 19:33:32 -0400

    Completely reorganized decoder and updated definitions where necessary. Does not try to provide direct ALU operands anymore. Added several cases for opcodes in decode.v. Made sure we did not use multiple signals with similar names that were intended to be the same signal. Not complete; will need to go through it again later, but it is good enough for now. Should be in a workable state.

--- Instruct_Decode/decode.v
+++ Instruct_Decode/decode.v
@@ -1,115 +1,185 @@
 `include "definitions.vh"
 
+// o_alu_a[1:0] and o_alu_b[1:0] are operand-select controls using the
+// ALU_A_* and ALU_B_* macros in definitions.vh. They remain DATA_WIDTH wide
+// only to preserve the original port names/shape while the rest of the CPU is
+// still being assembled.
 module decode (
-    input wire[`INST_WIDTH-1:0] inst,
+    input  wire [`INST_WIDTH-1:0] inst,
 
-    output reg [`FUNCT7_WIDTH-1:0] o_alu_op,
-    output reg [`DATA_WIDTH-1:0] o_alu_a,
-    output reg [`DATA_WIDTH-1:0] o_alu_b,
+    output reg  [`ALU_OP_WIDTH-1:0]   o_alu_op,
+    output reg  [`DATA_WIDTH-1:0]     o_alu_a,
+    output reg  [`DATA_WIDTH-1:0]     o_alu_b,
 
-    output reg o_branch,
-    output reg [2:0] o_branch_op,
-    output reg [`FUNCT7_WIDTH-1:0] o_opcode,
-    output reg [1:0] o_result_mux,
-    output reg o_reg_write,
-    output reg [$clog2(`NUM_REGISTER)-1:0] o_rs1_addr,
-    output reg [$clog2(`NUM_REGISTER)-1:0] o_rs2_addr,
-    output reg [$clog2(`NUM_REGISTER)-1:0] o_rd_addr,
+    output reg                        o_branch,
+    output reg  [2:0]                 o_branch_op,
+    output wire [6:0]                 o_opcode, //Don't think we need this, as opcode is a separate internal signal, but it will be kept for now, just in case.
+    output reg  [1:0]                 o_result_mux,
+    output reg                        o_reg_write,
+    output wire [`REG_ADDR_WIDTH-1:0] o_rs1_addr,
+    output wire [`REG_ADDR_WIDTH-1:0] o_rs2_addr,
+    output wire [`REG_ADDR_WIDTH-1:0] o_rd_addr,
+    output reg  [`DATA_WIDTH-1:0]     o_imm,
 
-    output reg o_mem_write
+    output reg                        o_mem_write
 );
-    // Preemptive field readings. Whether we need them or not depends on instruction
-    wire [6:0] o_opcode = inst[6:0]; //Instruction opcode
+    wire [6:0] opcode = inst[6:0];
     wire [4:0] rd     = inst[11:7];
     wire [2:0] funct3 = inst[14:12];
     wire [4:0] rs1    = inst[19:15];
     wire [4:0] rs2    = inst[24:20];
     wire [6:0] funct7 = inst[31:25];
 
-    // Immediates depending on kind of instruction
-    wire [31:0] imm_i = {{20{inst[31]}}, inst[31:20]};
-    wire [31:0] imm_s = {{20{inst[31]}}, inst[31:25], inst[11:7]};
-    wire [31:0] imm_b = {{19{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
-    wire [31:0] imm_u = {inst[31:12], 12'b0};
-    wire [31:0] imm_j = {{11{inst[31]}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
+    wire [`DATA_WIDTH-1:0] imm_i = {{20{inst[31]}}, inst[31:20]};
+    wire [`DATA_WIDTH-1:0] imm_s = {{20{inst[31]}}, inst[31:25], inst[11:7]};
+    wire [`DATA_WIDTH-1:0] imm_b = {{19{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
+    wire [`DATA_WIDTH-1:0] imm_u = {inst[31:12], 12'b0};
+    wire [`DATA_WIDTH-1:0] imm_j = {{11{inst[31]}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
 
-    assign o_rd_addr  = inst[11:7];
-    assign o_rs1_addr = inst[19:15];
-    assign o_rs2_addr = inst[24:20];
+    assign o_opcode   = opcode; //Remove if o_opcode is removed
+    assign o_rd_addr  = rd;
+    assign o_rs1_addr = rs1;
+    assign o_rs2_addr = rs2;
 
     always @* begin
-    o_alu_op     = `OP_ALU_NOP;
-    o_result_mux = 2'b00;
-    o_reg_write  = 1'b0;
-    o_mem_write  = 1'b0;
-    o_branch     = 1'b0;
-    o_branch_op  = 3'b000;
-    o_imm        = 32'b0;
+        // Safe defaults: unsupported instructions behave like a NOP.
+        o_alu_op     = `OP_ALU_NOP;
+        o_alu_a      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_RS1};
+        o_alu_b      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_RS2};
+        o_result_mux = `RESULT_ALU;
+        o_reg_write  = 1'b0;
+        o_mem_write  = 1'b0;
+        o_branch     = 1'b0;
+        o_branch_op  = 3'b000;
+        o_imm        = {`DATA_WIDTH{1'b0}};
 
-    case (o_opcode)
-        `Rtype: begin
-            o_reg_write = 1'b1;
+        case (opcode)
+            `Rtype: begin
+                o_reg_write = 1'b1;
+                o_alu_a     = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_RS1};
+                o_alu_b     = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_RS2};
+                o_result_mux = `RESULT_ALU;
 
-            case ({funct7, funct3})
-                {7'b0000000, 3'b000}: o_alu_op = `OP_ALU_ADD;
-                {7'b0100000, 3'b000}: o_alu_op = `OP_ALU_SUB;
-                {7'b0000000, 3'b111}: o_alu_op = `OP_ALU_AND;
-                {7'b0000000, 3'b110}: o_alu_op = `OP_ALU_OR;
-                {7'b0000000, 3'b100}: o_alu_op = `OP_ALU_XOR;
-                {7'b0000000, 3'b010}: o_alu_op = `OP_ALU_SLT;
-                {7'b0000000, 3'b011}: o_alu_op = `OP_ALU_SLTU;
-                {7'b0000000, 3'b001}: o_alu_op = `OP_ALU_SLL;
-                {7'b0000000, 3'b101}: o_alu_op = `OP_ALU_SRL;
-                {7'b0100000, 3'b101}: o_alu_op = `OP_ALU_SRA;
-                default: begin
-                end
-            endcase
-        end
+                case ({funct7, funct3})
+                    {7'b0000000, 3'b000}: o_alu_op = `OP_ALU_ADD;  // ADD
+                    {7'b0100000, 3'b000}: o_alu_op = `OP_ALU_SUB;  // SUB
+                    {7'b0000000, 3'b111}: o_alu_op = `OP_ALU_AND;  // AND
+                    {7'b0000000, 3'b110}: o_alu_op = `OP_ALU_OR;   // OR
+                    {7'b0000000, 3'b100}: o_alu_op = `OP_ALU_XOR;  // XOR
+                    {7'b0000000, 3'b010}: o_alu_op = `OP_ALU_SLT;  // SLT
+                    {7'b0000000, 3'b011}: o_alu_op = `OP_ALU_SLTU; // SLTU
+                    {7'b0000000, 3'b001}: o_alu_op = `OP_ALU_SLL;  // SLL
+                    {7'b0000000, 3'b101}: o_alu_op = `OP_ALU_SRL;  // SRL
+                    {7'b0100000, 3'b101}: o_alu_op = `OP_ALU_SRA;  // SRA
+                    default: begin
+                        o_reg_write = 1'b0;
+                        o_alu_op    = `OP_ALU_NOP;
+                    end
+                endcase
+            end
 
-        `Itype_A: begin
-            o_reg_write = 1'b1;
-            o_imm       = imm_i;
+            `Itype_A: begin
+                o_reg_write = 1'b1;
+                o_alu_a     = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_RS1};
+                o_alu_b     = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_IMM};
+                o_result_mux = `RESULT_ALU;
+                o_imm       = imm_i;
 
-            case (funct3)
-                3'b000: o_alu_op = `OP_ALU_ADD;  // ADDI
-                3'b010: o_alu_op = `OP_ALU_SLT;  // SLTI
-                3'b011: o_alu_op = `OP_ALU_SLTU; // SLTIU
-                3'b100: o_alu_op = `OP_ALU_XOR;  // XORI
-                3'b110: o_alu_op = `OP_ALU_OR;   // ORI
-                3'b111: o_alu_op = `OP_ALU_AND;  // ANDI
+                case (funct3)
+                    3'b000: o_alu_op = `OP_ALU_ADD;  // ADDI
+                    3'b010: o_alu_op = `OP_ALU_SLT;  // SLTI
+                    3'b011: o_alu_op = `OP_ALU_SLTU; // SLTIU
+                    3'b100: o_alu_op = `OP_ALU_XOR;  // XORI
+                    3'b110: o_alu_op = `OP_ALU_OR;   // ORI
+                    3'b111: o_alu_op = `OP_ALU_AND;  // ANDI
+                    3'b001: begin
+                        if (funct7 == 7'b0000000)
+                            o_alu_op = `OP_ALU_SLL;  // SLLI
+                        else
+                            o_reg_write = 1'b0;
+                    end
+                    3'b101: begin
+                        if (funct7 == 7'b0000000)
+                            o_alu_op = `OP_ALU_SRL;  // SRLI
+                        else if (funct7 == 7'b0100000)
+                            o_alu_op = `OP_ALU_SRA;  // SRAI
+                        else
+                            o_reg_write = 1'b0;
+                    end
+                    default: o_reg_write = 1'b0;
+                endcase
+            end
 
-                3'b001: begin
-                    if (funct7 == 7'b0000000)
-                        o_alu_op = `OP_ALU_SLL;  // SLLI
-                end
+            `Itype_L: begin
+                // Address = rs1 + imm_i; writeback comes from data memory.
+                o_reg_write  = 1'b1;
+                o_alu_op     = `OP_ALU_ADD;
+                o_alu_a      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_RS1};
+                o_alu_b      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_IMM};
+                o_result_mux = `RESULT_MEM;
+                o_imm        = imm_i;
+            end
 
-                3'b101: begin
-                    if (funct7 == 7'b0000000)
-                        o_alu_op = `OP_ALU_SRL;  // SRLI
-                    else if (funct7 == 7'b0100000)
-                        o_alu_op = `OP_ALU_SRA;  // SRAI
-                end
-            endcase
-        end
+            `Stype: begin
+                // Address = rs1 + imm_s; memory write data comes from rs2.
+                o_mem_write = 1'b1;
+                o_alu_op    = `OP_ALU_ADD;
+                o_alu_a     = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_RS1};
+                o_alu_b     = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_IMM};
+                o_imm       = imm_s;
+            end
 
-        `Stype: begin
-            o_mem_write = 1'b1;
-            o_imm       = imm_s;
-        end
+            `Btype: begin
+                o_branch    = 1'b1;
+                o_branch_op = funct3;
+                o_alu_a     = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_RS1};
+                o_alu_b     = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_RS2};
+                o_imm       = imm_b;
+            end
 
-        `Btype: begin
-            o_branch    = 1'b1;
-            o_branch_op = funct3;
-            o_imm       = imm_b;
-        end
+            `Utype: begin
+                // LUI: write upper immediate. The ALU is not required for this path.
+                o_reg_write  = 1'b1;
+                o_result_mux = `RESULT_IMM;
+                o_imm        = imm_u;
+            end
 
-        `Jtype: begin
-            o_branch    = 1'b1;
-            o_branch_op = `BRANCH_JAL_JALR;
-            o_reg_write = 1'b1;
-            o_imm       = imm_j;
-        end
-    endcase
-end
+            `Utype_AUIPC: begin
+                // AUIPC: rd = pc + imm_u.
+                o_reg_write  = 1'b1;
+                o_alu_op     = `OP_ALU_ADD;
+                o_alu_a      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_PC};
+                o_alu_b      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_IMM};
+                o_result_mux = `RESULT_ALU;
+                o_imm        = imm_u;
+            end
 
+            `Jtype: begin
+                // JAL: branch target is pc + imm_j; writeback is pc + 4.
+                o_branch     = 1'b1;
+                o_branch_op  = `BRANCH_JAL_JALR;
+                o_reg_write  = 1'b1;
+                o_alu_a      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_PC};
+                o_alu_b      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_FOUR};
+                o_result_mux = `RESULT_PC4;
+                o_imm        = imm_j;
+            end
+
+            `Itype_JALR: begin
+                // JALR: target is rs1 + imm_i; writeback is pc + 4.
+                o_branch     = 1'b1;
+                o_branch_op  = `BRANCH_JAL_JALR;
+                o_reg_write  = 1'b1;
+                o_alu_op     = `OP_ALU_ADD;
+                o_alu_a      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_A_RS1};
+                o_alu_b      = {{(`DATA_WIDTH-2){1'b0}}, `ALU_B_IMM};
+                o_result_mux = `RESULT_PC4;
+                o_imm        = imm_i;
+            end
+
+            default: begin
+                // Should keep the safe defaults that were defined above.
+            end
+        endcase
+    end
 endmodule
--- definitions.vh
+++ definitions.vh
@@ -1,61 +1,81 @@
-// Testbench utilities:
+`ifndef DEFINITIONS_VH // If DEFINITIONS_VH is not defined, define it as follows.
+`define DEFINITIONS_VH // Apparently considered good practice in larger projects so as to avoid defining it multiple times.
+
+// Testbench utilities
 `define assert(signal, value) \
-if (signal !== value) $finish;
+if ((signal) !== (value)) begin \
+    $display("FAIL: %s expected=%h got=%h time=%0t", \
+    `"signal`", value, signal, $time); \// Debugging has been fine so far, but this might be easier than manually checking the tb for expected and actual values
+    $finish; \
+    end
 
-`define DATA_WIDTH 32
-`define INST_WIDTH 32
-`define NUM_REGISTER 32
-`define FUNCT7_WIDTH 7
+    // Various constants so that the rationale for length is immediately (or quickly) apparent
+    `define DATA_WIDTH 32
+    `define INST_WIDTH 32
+    `define NUM_REGISTER 32
+    `define REG_ADDR_WIDTH 5
+    `define ALU_OP_WIDTH 7
+    `define FUNCT7_WIDTH 7
 
-
-// Instruction opcodes (identify type of instruction being fetched)
-//From included image of RV32I base instruction set
-`define Rtype       7'b0110011
-`define Itype_A     7'b0010011 //Arithmetic
-`define Itype_L     7'b0000011 //Load
-`define Stype       7'b0100011
-`define Btype       7'b1100011
-`define Utype       7'b0110111
-`define Jtype       7'b1101111
-`define Utype_AUIPC 7'b0010111 // Add upper immediate to PC
-`define Itype_JALR  7'b1100111
-`define Utype_AUIPC 7'b0010111
-`define Itype_JALR  7'b1100111
-`define Itype_SYS   7'b1110011
-//`define Itype_FENCE 7'b0001111 //Not really necessary as of the current implementation
-
-
-//Branch opcodes
-`define BRANCH_BEQ 3'b000 // Branch Equal
-`define BRANCH_BNE 3'b001 // Branch Not Equal
-`define BRANCH_BLT 3'b100 // Branch Less Than
-`define BRANCH_BGE 3'b101 // Branch Greater Than Or Equal
-`define BRANCH_BLTU 3'b110 // Branch Less Than Unsigned
-`define BRANCH_BGEU 3'b111 // Branch Greater Than Or Equal Unsigned
-`define BRANCH_JAL_JALR 3'b010 // Jump in case of JAL or JALR instruction
-// opcodes above are from funct3 field. Be careful with 010. It is not specified as a branch code in specs
+    // Branch opcodes. Conditional branch values match funct3.
+    `define BRANCH_BEQ 3'b000 // Branch Equal
+    `define BRANCH_BNE 3'b001 // Branch Not Equal
+    `define BRANCH_BLT 3'b100 // Branch Less Than
+    `define BRANCH_BGE 3'b101 // Branch Greater Than Or Equal
+    `define BRANCH_BLTU 3'b110 // Branch Less Than Unsigned
+    `define BRANCH_BGEU 3'b111 // Branch Greater Than Or Equal Unsigned
+    `define BRANCH_JAL_JALR 3'b010 // Jump in case of JAL or JALR instruction
+    // opcodes above are from funct3 field. Be careful with 010. It is not specified as a branch code in specs
 
 
-// Note on operation codes for ALU
-// First four bits define class (arithmetic, logic, and, for lack of better terminology, setting operations (abs, SLT, SLL, etc))
-// Latter four bits specify sub operation within each class (add, sub, and, etc)
+    // RV32I opcodes
+    `define Rtype       7'b0110011
+    `define Itype_A     7'b0010011 // Integer register-immediate ALU ops
+    `define Itype_L     7'b0000011 // Loads
+    `define Stype       7'b0100011 // Stores
+    `define Btype       7'b1100011 // Conditional branches
+    `define Utype       7'b0110111 // LUI
+    `define Utype_AUIPC 7'b0010111 // AUIPC
+    `define Jtype       7'b1101111 // JAL
+    `define Itype_JALR  7'b1100111 // JALR
+    `define Itype_SYS   7'b1110011 // SYSTEM; currently decoded as unsupported/NOP
 
-//ALU ops
-`define OP_ALU_NOP    7'b0000000 // Zero/"Universal Identity" operation
-`define OP_ALU_ADD    7'b0010001 // Add
-`define OP_ALU_SUB    7'b0010010 // Subtract
-`define OP_ALU_AND    7'b0100001 // Bitwise AND
-`define OP_ALU_OR     7'b0100010 // Bitwise OR
-`define OP_ALU_XOR    7'b0100011 // Bitwise XOR
-`define OP_ALU_SLT    7'b0110001 // Set Less Than (signed)
-`define OP_ALU_SLTU   7'b0110010 // Set Less Than (unsigned)
-`define OP_ALU_SLL    7'b0110011 // Shift Left Logical
-`define OP_ALU_SRL    7'b0110100 // Shift Right Logical
-`define OP_ALU_SRA    7'b0110101 // Shift Right Arithmetic
+    // ALU operand-A select. Values are placed in o_alu_a[1:0].
+    `define ALU_A_RS1 2'b00
+    `define ALU_A_PC  2'b01
+    `define ALU_A_ZERO 2'b10
 
-// Additional operations outside basics (though still pretty basic). Includes RV32M operations (MUL,DIV) and custom operations
-// Will likely be expanded later
-`define OP_ALU_INV    7'b0100100 // Bitwise Inversion (NOT operation)
-`define OP_ALU_MUL    7'b0010011 // Multiplication
-`define OP_ALU_DIV    7'b0010100 // Division
-`define OP_ALU_MOD    7'b0010101 // Modulus
+    // ALU operand-B select. Values are placed in o_alu_b[1:0].
+    `define ALU_B_RS2 2'b00
+    `define ALU_B_IMM 2'b01
+    `define ALU_B_FOUR 2'b10
+
+    // Writeback/result mux select.
+    `define RESULT_ALU 2'b00
+    `define RESULT_MEM 2'b01
+    `define RESULT_PC4 2'b10
+    `define RESULT_IMM 2'b11
+
+    // ALU ops
+    // Note: first three bits define type (arithmetic, logic, other)
+    // Latter four bits specify specific operation within type.
+    `define OP_ALU_NOP    7'b0000000 // Zero/"Universal Identity" operation
+    `define OP_ALU_ADD    7'b0010001 // Add
+    `define OP_ALU_SUB    7'b0010010 // Subtract
+    `define OP_ALU_AND    7'b0100001 // Bitwise AND
+    `define OP_ALU_OR     7'b0100010 // Bitwise OR
+    `define OP_ALU_XOR    7'b0100011 // Bitwise XOR
+    `define OP_ALU_SLT    7'b0110001 // Set Less Than (signed)
+    `define OP_ALU_SLTU   7'b0110010 // Set Less Than (unsigned)
+    `define OP_ALU_SLL    7'b0110011 // Shift Left Logical
+    `define OP_ALU_SRL    7'b0110100 // Shift Right Logical
+    `define OP_ALU_SRA    7'b0110101 // Shift Right Arithmetic
+
+    // Additional operations outside basics (though still pretty basic). Includes RV32M operations (MUL,DIV) and custom operations
+    // Will likely be expanded later
+    `define OP_ALU_INV    7'b0100100
+    `define OP_ALU_MUL    7'b0010011
+    `define OP_ALU_DIV    7'b0010100
+    `define OP_ALU_MOD    7'b0010101
+
+    `endif// Put all future additions to the file above here.
