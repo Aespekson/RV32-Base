edeef15a4bad27d04144b7b7a047d9528f073389
aespekson <john6610@yandex.com>
2026-06-24 16:44:14 -0400

    Forgot to add sensitivity list for always_comb block. Also renamed rd and rd_addr to write_data and write_addr respectively. Changed logic to reg. Made sure rst has priority. Also changed names in tb and resimulated.

--- Register_File/register_file.v
+++ Register_File/register_file.v
@@ -4,18 +4,18 @@ module register_file(
   input wire clk,
   input wire we,
   input wire rst,
-  input wire [$clog2(`NUM_REGISTER)-1:0] rd_addr,
-  input wire [`DATA_WIDTH-1:0] rd,
+  input wire [$clog2(`NUM_REGISTER)-1:0] write_addr,
+  input wire [`DATA_WIDTH-1:0] write_data,
   input wire [$clog2(`NUM_REGISTER)-1:0] rs1_addr,
   input wire [$clog2(`NUM_REGISTER)-1:0] rs2_addr,
-  output logic [`DATA_WIDTH-1:0] rs1,
-  output logic [`DATA_WIDTH-1:0] rs2);
+  output reg [`DATA_WIDTH-1:0] rs1,
+  output reg [`DATA_WIDTH-1:0] rs2);
 
 
-  logic [`DATA_WIDTH-1:0] registers [`NUM_REGISTER-1:0];
+  reg [`DATA_WIDTH-1:0] registers [`NUM_REGISTER-1:0];
   integer i;
 
-  always begin
+  always @* begin
     if (rs1_addr == 0) rs1 = 0;
     else rs1 = registers[rs1_addr];
     if (rs2_addr == 0) rs2 = 0; // x0 hardwired to 0
@@ -24,9 +24,10 @@ module register_file(
 
   always @(posedge clk) begin
     if (rst) begin
-      for (int i = 0; i < `NUM_REGISTER; i++) registers[i]<= 32'h00000000;
-    end
-    if (we && rd_addr!=0) registers[rd_addr] <= rd;
+      for (int i = 0; i < `NUM_REGISTER; i++)
+        registers[i]<= 32'h00000000;
+    end else if (we && write_addr!=0)
+      registers[write_addr] <= write_data;
   end
 
 endmodule
--- Register_File/register_file_tb.v
+++ Register_File/register_file_tb.v
@@ -10,11 +10,11 @@ module register_file_tb;
   reg we;
   reg rst;
 
-  reg [$clog2(`NUM_REGISTER)-1:0] rd_addr = 5'b00000;
+  reg [$clog2(`NUM_REGISTER)-1:0] write_addr = 5'b00000;
   reg [$clog2(`NUM_REGISTER)-1:0] rs1_addr = 5'b00000;
   reg [$clog2(`NUM_REGISTER)-1:0] rs2_addr = 5'b00000;
 
-  reg [`DATA_WIDTH-1:0] rd = 32'h00000000;
+  reg [`DATA_WIDTH-1:0] write_data = 32'h00000000;
   wire [`DATA_WIDTH-1:0] rs1;
   wire [`DATA_WIDTH-1:0] rs2;
 
@@ -24,8 +24,8 @@ register_file dut (
     .clk(clk),
     .we(we),
     .rst(rst),
-    .rd_addr(rd_addr),
-    .rd(rd),
+    .write_addr(write_addr),
+    .write_data(write_data),
     .rs1_addr(rs1_addr),
     .rs2_addr(rs2_addr),
     .rs1(rs1),
@@ -46,12 +46,12 @@ register_file dut (
 
   //The following has been added because the original test register intialization (kept in an initial block for simulation only) was moved to the always_ff block for synthesizability, and as such, the read will be tested after we have written the data via the tb. This, unfortunately, does not test the read independently of the write, but it was tested before, and the logic is small enough that it should be perfectly fine.
   @(negedge clk)
-    we = 1'b1;// rd_addr does not change independently of rd, so there should be no danger of keeping write enabled until the end.
-    rd_addr = 5'b00001;
-    rd = 2;
+    we = 1'b1;// write_addr does not change independently of write_data, so there should be no danger of keeping write enabled until the end.
+    write_addr = 5'b00001;
+    write_data = 2;
   @(negedge clk)
-    rd_addr = 5'b00010;
-    rd = 3;
+    write_addr = 5'b00010;
+    write_data = 3;
   // End of writing for read testing.
 
   @(negedge clk)
@@ -62,8 +62,8 @@ register_file dut (
     `assert(dut.rs2,3)
     rs1_addr = 5'b11111;
     rs2_addr = 5'b00000;
-    rd_addr = 5'b11111;
-    rd = 32'h00000010;
+    write_addr = 5'b11111;
+    write_data = 32'h00000010;
   @(negedge clk);
     `assert(dut.rs1,32'h00000010);
     `assert(dut.rs2,32'h00000000);
