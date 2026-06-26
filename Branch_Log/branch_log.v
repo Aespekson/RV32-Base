1271ad405ea906cc2e2b6fbb14bacd96e2adbb16
aespekson <john6610@yandex.com>
2026-06-26 13:20:09 -0400

    Comment clarification.

--- Branch_Log/branch_log.v
+++ Branch_Log/branch_log.v
@@ -2,14 +2,14 @@
 `include "definitions.vh"
 
 module branch_log ( // In short, determines whether a branch should be taken based on certain conditions. The condition is specified via operation code, similar to ALU, and evaluated based on operands a and b.
-    input wire i_branch, // Effectively an enable signal
+    input wire i_branch, // Enable signal
     input wire [2:0] i_branch_op,
     input wire [`DATA_WIDTH-1:0] i_a,
     input wire [`DATA_WIDTH-1:0] i_b,
     output reg o_take //Take branch or no
 );
 
-wire signed [`DATA_WIDTH-1:0] i_a_sin = i_a; // I believe these both function as aliases, effectively. If not, will need to update inside the block
+wire signed [`DATA_WIDTH-1:0] i_a_sin = i_a;
 wire signed [`DATA_WIDTH-1:0] i_b_sin = i_b;
 
     always @* begin
@@ -53,7 +53,7 @@ wire signed [`DATA_WIDTH-1:0] i_b_sin = i_b;
                 end
 
               	`BRANCH_JAL_JALR: begin //Upon receiving appropriate instruction from deocde, will take branch, as is in-line with the instruction's use case.
-              	//For JALR, later, remember the address jumped to is determined by ALU. Do not forget to wire that up.
+              	//For JALR, later, remember the address jumped to is determined by ALU. Do not forget to wire that up. // Upon review, this is optional. It would lead to deduplication of hardware (only using one adder in the ALU rather than using a second elsewhere) but is not strictly necessary.
                     o_take = 1;
                 end
                 default: o_take = 0;
