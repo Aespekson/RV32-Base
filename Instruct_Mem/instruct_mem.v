ba70835093fb57820a5df09d1c4b754d06a66590
aespekson <john6610@yandex.com>
2026-06-27 14:45:45 -0400

    Added comments to test program 1.

--- Instruct_Mem/instruct_mem.v
+++ Instruct_Mem/instruct_mem.v
@@ -15,11 +15,13 @@ module instruction_memory #(
         // Fill other addresses
         for (i = 0; i < `INST_MEM_DEPTH; i = i + 1)
             memory[i] = 32'h00000013; // addi x0, x0, 0
-
-        memory[0] = 32'h00108113; // addi x2, x1, 1
-        memory[1] = 32'h00108193; // addi x3, x1, 1
-        memory[2] = 32'h00310233; // add  x4, x2, x3
-        memory[3] = 32'hfe218ae3; // beq  x3, x2, -12
+/*
+        // Test program 1. Uncomment to load.
+        memory[0] = 32'h00108113; // addi x2, x1, 1   // Adds value at x1 to 1 and stores at x2
+        memory[1] = 32'h00108193; // addi x3, x1, 1   // Adds value at x1 to 1 and stores at x3
+        memory[2] = 32'h00310233; // add  x4, x2, x3  // Adds x2 and x3 and stores at x4
+        memory[3] = 32'hfe218ae3; // beq  x3, x2, -12 // Offsets PC by -12 if values at x3 and x2 equal.
+*/
     end
 
     always @* begin
